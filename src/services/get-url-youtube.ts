bdee1b17a5dac4c42c694c737e93a0d68a5e1025
rosch-arthur <rosch.arthur@gmail.com>
2024-09-02 20:35:52 -0300

    Feat: Add Console Erro em Get url Yt Dl

--- src/services/get-url-youtube.ts
+++ src/services/get-url-youtube.ts
@@ -2,18 +2,25 @@ const youtubedl = require('youtube-dl-exec')
 const logger = require('progress-estimator')()
 
 export async function getUrlYoutube(url: string) {
-  const promise = youtubedl(url, {
-    dumpSingleJson: true,
-    noCheckCertificates: true,
-    noWarnings: true,
-    preferFreeFormats: true,
-    addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
-  })
-  const result = await logger(promise, `Obtaining ${url}`)
-  console.log(result)
-  const videoFormat = result.requested_downloads.find(
-    (format) => format.ext === 'mp4',
-  )
+  try {
+    const promise = youtubedl(url, {
+      dumpSingleJson: true,
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+    })
 
-  return videoFormat.url
+    const result = await logger(promise, `Obtaining ${url}`)
+    console.log(result)
+
+    const videoFormat = result.requested_downloads.find(
+      (format) => format.ext === 'mp4',
+    )
+
+    return videoFormat.url
+  } catch (error) {
+    console.error('Error fetching video URL:', error)
+    throw new Error(`Failed to obtain video URL for ${url}`)
+  }
 }
