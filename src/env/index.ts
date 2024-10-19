491354798be0b59fde50387cd01b706dcf775538
higorxi <higorgiovane@ucashapp.com>
2024-10-19 04:15:51 -0300

    fix bug in type for database url

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url().nonempty(),
+  DATABASE_URL: z.string().nonempty(),
   JWT_SECRET: z.string().nonempty(),
   DB_HOST: z.string().nonempty(),
   DB_PORT: z.coerce.number().default(25060),
