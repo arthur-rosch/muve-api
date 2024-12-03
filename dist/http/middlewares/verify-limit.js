1a950d2705e65154e67fc958deb5d25445a6631e
rosch-arthur <rosch.arthur@gmail.com>
2024-12-03 15:24:59 -0300

    Fix responses error ins moddlewares

--- dist/http/middlewares/verify-limit.js
+++ dist/http/middlewares/verify-limit.js
@@ -17,7 +17,7 @@ const checkVideoLimitMiddleware = (request, reply) => __awaiter(void 0, void 0, 
     var _a;
     const userId = (_a = request.user) === null || _a === void 0 ? void 0 : _a.sub;
     if (!userId) {
-        return reply.status(401).send({ message: 'Usuário não autenticado.' });
+        return reply.status(401).send({ message: 'Unauthorized' }); // Correspondente ao front
     }
     try {
         const signature = yield prisma.signature.findFirst({
@@ -28,25 +28,27 @@ const checkVideoLimitMiddleware = (request, reply) => __awaiter(void 0, void 0, 
                 created_at: 'desc',
             },
         });
+        if (!signature) {
+            return reply.status(401).send({ message: 'Subscription not found.' }); // Correspondente ao front
+        }
         if (signature.status === 'canceled') {
-            return reply.status(403).send({ message: 'Assinatura cancelada.' });
+            return reply.status(403).send({ message: 'Subscription cancelled.' }); // Correspondente ao front
         }
         if (signature.status === 'past_due') {
-            return reply
-                .status(403)
-                .send({ message: 'Assinatura com pagamento atrasado.' });
+            return reply.status(403).send({ message: 'Late subscription.' }); // Correspondente ao front
         }
         if (signature.status === 'trialing') {
             const trialEndDate = signature.trial_end_date;
             if (trialEndDate && new Date(trialEndDate) < new Date()) {
-                return reply.status(403).send({ message: 'Período de teste expirado.' });
+                return reply.status(403).send({ message: 'Trial expired.' }); // Correspondente ao front
             }
         }
-        if (signature.status !== 'active' && signature.status !== 'trialing') {
-            return reply.status(403).send({ message: 'Assinatura inválida.' });
+        if (signature.status !== 'active' &&
+            signature.status !== 'trialing' &&
+            signature.status !== 'free') {
+            return reply.status(403).send({ message: 'Invalid subscription.' }); // Correspondente ao front
         }
         const planName = (0, utils_1.planNameMappingStripe)(signature.plan);
-        console.log(planName);
         let videoLimit;
         switch (planName) {
             case 'Mensal - Essencial':
@@ -67,12 +69,12 @@ const checkVideoLimitMiddleware = (request, reply) => __awaiter(void 0, void 0, 
             },
         });
         if (videoCount >= videoLimit) {
-            return reply.status(403).send({ message: 'Limite de vídeos excedido.' });
+            return reply.status(403).send({ message: 'Video limit exceeded.' }); // Correspondente ao front
         }
     }
     catch (error) {
         console.error('Erro ao verificar limite de vídeos:', error);
-        reply.status(500).send({ message: 'Erro interno do servidor.' });
+        reply.status(500).send({ message: 'Internal server error.' }); // Correspondente ao front
     }
 });
 exports.checkVideoLimitMiddleware = checkVideoLimitMiddleware;
--- dist/http/middlewares/verify-signature.js
+++ dist/http/middlewares/verify-signature.js
@@ -16,7 +16,7 @@ const checkSignatureMiddleware = (request, reply) => __awaiter(void 0, void 0, v
     var _a;
     const userId = (_a = request.user) === null || _a === void 0 ? void 0 : _a.sub;
     if (!userId) {
-        return reply.status(401).send({ message: 'Usuário não autenticado.' });
+        return reply.status(401).send({ message: 'Unauthorized' }); // Correspondente ao front
     }
     try {
         const signature = yield prisma.signature.findFirst({
@@ -28,31 +28,29 @@ const checkSignatureMiddleware = (request, reply) => __awaiter(void 0, void 0, v
             },
         });
         if (!signature) {
-            return reply.status(401).send({ message: 'Usuário sem Plano' });
+            return reply.status(401).send({ message: 'Subscription not found.' }); // Correspondente ao front
         }
         if (signature.status === 'canceled') {
-            return reply.status(403).send({ message: 'Assinatura cancelada.' });
+            return reply.status(403).send({ message: 'Subscription cancelled.' }); // Correspondente ao front
         }
         if (signature.status === 'past_due') {
-            return reply
-                .status(403)
-                .send({ message: 'Assinatura com pagamento atrasado.' });
+            return reply.status(403).send({ message: 'Late subscription.' }); // Correspondente ao front
         }
         if (signature.status === 'trialing') {
             const trialEndDate = signature.trial_end_date;
             if (trialEndDate && new Date(trialEndDate) < new Date()) {
-                return reply.status(403).send({ message: 'Período de teste expirado.' });
+                return reply.status(403).send({ message: 'Trial expired.' }); // Correspondente ao front
             }
         }
         if (signature.status !== 'active' &&
             signature.status !== 'trialing' &&
             signature.status !== 'free') {
-            return reply.status(403).send({ message: 'Assinatura inválida.' });
+            return reply.status(403).send({ message: 'Invalid subscription.' }); // Correspondente ao front
         }
     }
     catch (error) {
         console.error('Erro ao verificar assinatura:', error);
-        reply.status(500).send({ message: 'Erro interno do servidor.' });
+        reply.status(500).send({ message: 'Internal server error.' }); // Correspondente ao front
     }
 });
 exports.checkSignatureMiddleware = checkSignatureMiddleware;
